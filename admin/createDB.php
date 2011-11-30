f13c349ae1de4865c49f7792b785814b9bf60201
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2011-11-30 17:25:26 -0500

    WIP

--- admin/createDB.php
+++ admin/createDB.php
@@ -1,31 +1,60 @@
 <?php        
-        // parse file to get password
-        $cnf = parse_ini_file(".my.cnf");
-        
-        //connect to DB with info from my.conf
-        try
-        {
-            $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-            $bdd = new PDO("mysql:host=".$cnf['host'].";dbname=".$cnf['database'],
-                           $cnf['user'],$cnf['password'], $pdo_options);
-            
-            // insert new user
-            //$reponse = $bdd->query('CREATE TABLE users (id  int NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(32), email VARCHAR(32), password VARCHAR(32));');
-            $reponse = $bdd->query("INSERT INTO users(name, email, password) VALUES('nicol2as2', 'nico2lasemail2', 'password22')");
-            
-            // On affiche chaque entrée une à une
-            while ($donnees = $reponse->fetch())
-            {
-                echo '-'.$donnees[Tables_in_imagebank];
-            }
+    // parse file to get password
+    $cnf = parse_ini_file(".my.cnf");
     
-            $reponse->closeCursor(); // Termine le traitement de la requête
-            
-            //back to main page
-            header('Location: admin.php');
-        }
-        catch (Exception $e)
-        {
-            die('Error : ' . $e->getMessage());
-        }
+    $host=$cnf['host']; // Host name 
+    $username=$cnf['user']; // Mysql username 
+    $password=$cnf['password']; // Mysql password 
+    $db_name=$cnf['database']; // Database name 
+    $tbl_name="users"; // Table name
+    
+    // Connect to server and select databse.
+    $con = mysql_connect("$host", "$username", "$password")or die("cannot connect");
+    if (!$con)
+    {
+        die('Could not connect: ' . mysql_error());
+    }
+    
+    // select DB
+    mysql_select_db("$db_name")or die("cannot select DB");
+    
+    // assume db exists (create from phpmyadmin with user
+    // database should exist
+    // create tables
+    $sql="CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT, 
+    PRIMARY KEY(id),
+    name VARCHAR(30), 
+    email VARCHAR(30),
+    password VARCHAR(30))";
+    
+    if (mysql_query($sql, $con))
+    {
+        echo "Table users created";
+    }
+    else
+    {
+        echo "Error creating table users: " . mysql_error();
+    }
+    
+    // create tables
+    $sql="CREATE TABLE images(
+    id INT NOT NULL AUTO_INCREMENT, 
+    PRIMARY KEY(id),
+    owner INT, 
+    name VARCHAR(30),
+    modified VARCHAR(30),
+    type VARCHAR(30),
+    date VARCHAR(30))";
+    
+    if (mysql_query($sql, $con))
+    {
+        echo "Table images created";
+    }
+    else
+    {
+        echo "Error creating table images: " . mysql_error();
+    }
+    
+    mysql_close($con);
 ?>
--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
     
-    if (isset($_POST['login']) AND isset($_POST['pass']))
+    if (isset($_POST['login']) AND isset($_POST['pass']) AND isset($_POST['email']))
     {
         // parse file to get password
         $cnf = parse_ini_file("admin/.my.cnf");
@@ -16,19 +16,61 @@
         mysql_select_db("$db_name")or die("cannot select DB");
         
         // username and password sent from form 
-        $myusername=$_POST['myusername']; 
-        $mypassword=$_POST['mypassword'];
+        $myusername=$_POST['login']; 
+        $mypassword=$_POST['pass'];
+        $myemail=$_POST['email'];
         
         // To protect MySQL injection (more detail about MySQL injection)
         $myusername = stripslashes($myusername);
         $mypassword = stripslashes($mypassword);
+        $myemail = stripslashes($myemail);
         $myusername = mysql_real_escape_string($myusername);
         $mypassword = mysql_real_escape_string($mypassword);
+        $myemail = mysql_real_escape_string($myemail);
+        
+        //
+        $sql="select * from users where name=$myusername";
+        
+        if (!mysql_query($sql, $con))
+        {
+            echo "User doesnt not exist, let's create one!";
+            //$sql2="INSERT INTO users(name, email, password) VALUES('name', 'email', 'pass')";
+            //mysql_query($sql2, $con);
+        }
+        else
+        {
+            echo "User already exists";
+        }
+        
+        // password md5()
+        
+        // create folders
+        /* Step 1. I need to know the absolute path to where I am now, ie where this script is running from...*/ 
+        //$thisdir = getcwd(); 
+        
+        /* Step 2. From this folder, I want to create a subfolder called "myfiles".  Also, I want to try and make this folder world-writable (CHMOD 0777). Tell me if success or failure... */
+        
+        /*$path = $thisdir . "/bank/hello";
+        echo "$path";
+        
+        if(mkdir( $path, 0777)) 
+        { 
+            echo "Directory has been created successfully..."; 
+        } 
+        else 
+        { 
+            $error = error_get_last();
+            echo $error['message'];
+            echo "Failed to create directory..."; 
+            
+            
+        }*/
+        
         
         //$sql="SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
-        $sql="INSERT INTO users(name, email, password) VALUES('nicol2as2', 'nico2lasemail2', 'password22')";
+        $sql="INSERT INTO users(name, email, password) VALUES('$myusername', '$myemail', '$mypassword')";
         $result=mysql_query($sql);
-        
+        /*
         // Mysql_num_row is counting table row
         $count=mysql_num_rows($result);
         // If result matched $myusername and $mypassword, table row must be 1 row
@@ -42,7 +84,7 @@
         else {
             echo "Wrong Username or Password";
         }
-    
+    */
         //connect to DB with info from my.conf
         /*try
         {
