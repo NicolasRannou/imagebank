9d3fcc3b0821c544d86f19bd0257bddb2c0864f7
Nicolas Rannou <nicolas_rannou@hms.harvard.edu>
2011-12-01 08:17:58 -0500

    WIP: login/start session works
    todo: encrypt password
    todo: modify admin pass
    todo: registered page
    (upload, visu, process)
    todo: logout

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,6 @@
 <form id='createDB' action='createDB.php' method='forgot' accept-charset='UTF-8'>
-<button type="submit"><b><i>createDB</i></b></button>
+<button type="submit"><b><i>Create Database</i></b></button>
+</form>
+<form id='modifyAdminPass' action='modifyAdminPass.php' method='forgot' accept-charset='UTF-8'>
+<button type="submit"><b><i>Modify Admin Password</i></b></button>
 </form>
--- login.php
+++ login.php
@@ -1,26 +1,42 @@
-<?php echo 'in login';
-    if ($_POST['username'] == 'admin')
-    {
-        echo 'admin';
-        // start session with passwd
-        // go the the admin page
-    }
-    else
-    {
-        echo 'user';
-        // start session with pass
-        // go to logged page
-        // change password
-        // show all its images (checkable)
-        // upload button
-        // list filters of imageJ
-        //else go back to main menu
-    }
+<?php
+    // parse file to get password
+    $cnf = parse_ini_file("admin/.my.cnf");
     
+    $host=$cnf['host']; // Host name 
+    $username=$cnf['user']; // Mysql username 
+    $password=$cnf['password']; // Mysql password 
+    $db_name=$cnf['database']; // Database name 
+    $tbl_name="users"; // Table name
+        
+    // Connect to server and select databse.
+    $con = mysql_connect("$host", "$username", "$password")or die("cannot connect");
+    mysql_select_db("$db_name", $con)or die("cannot select DB");
     
-    ?>
-// check if user name == admin -> create db page
-
-// else test mysql with user and (encrypted?) password
-// start session
-// else go back to login
+    // username and password sent from form
+    $mypassword=$_POST['password'];
+    $myemail=$_POST['email'];
+        
+    // To protect MySQL injection (more detail about MySQL injection)
+    $mypassword = stripslashes($mypassword);
+    $myemail = stripslashes($myemail);
+    $mypassword = mysql_real_escape_string($mypassword);
+    $myemail = mysql_real_escape_string($myemail);
+        
+    $sql="SELECT * FROM $tbl_name WHERE email='$myemail' and password='$mypassword'";
+    $result=mysql_query($sql);
+    
+    // Mysql_num_row is counting table row
+    $count=mysql_num_rows($result);
+    // If result matched $myemail and $mypassword, table row must be 1 row
+    if($count==1){
+        // Register $myusername, $mypassword and $myemail and redirect to file "login_success.php"
+        session_register("myemail");
+        session_register("myusername");
+        session_register("mypassword");
+        header("location:registeredarea.php");
+    }
+    else {
+        echo "Wrong Username or Password";
+        echo "<meta http-equiv='refresh' content='3;url=index.php'>";
+    }
+?>
